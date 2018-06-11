87ea4711338b531557c6ed1962d762ab979da501
Laurent Curau <laurentcurau@gmail.com>
2018-06-11 17:07:41 +0100

    Player can mark the board

--- tests/board.test.js
+++ tests/board.test.js
@@ -5,3 +5,9 @@ it('is empty', () => {
   var board = new Board();
   expect(board.isEmpty()).to.equal(true);
 });
+
+it('marks a position', () => {
+  var board = new Board();
+  board.markCellAtPosition(1, "X");
+  expect(board.isEmpty()).to.equal(false);
+});
