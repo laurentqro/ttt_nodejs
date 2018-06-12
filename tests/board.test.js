b1352b80f51eaea72756b2cb315681b5ee3d43c3
Laurent Curau <laurentcurau@gmail.com>
2018-06-12 12:46:26 +0100

    Board identifies win and tie

--- tests/board.test.js
+++ tests/board.test.js
@@ -8,6 +8,52 @@ it('is empty', () => {
 
 it('marks a position', () => {
   var board = new Board();
-  board.markCellAtPosition(1, "X");
+  board.markCellAtPosition(1, 'X');
+
   expect(board.isEmpty()).to.equal(false);
+  expect(board.getCellAtPosition(1).getSymbol()).to.equal('X');
+});
+
+it('identifies no win', () => {
+  var board = new Board();
+
+  board.markCellAtPosition(1, 'X');
+  board.markCellAtPosition(2, 'X');
+  board.markCellAtPosition(6, 'X');
+  board.markCellAtPosition(7, 'X');
+  board.markCellAtPosition(9, 'X');
+
+  board.markCellAtPosition(3, 'O');
+  board.markCellAtPosition(4, 'O');
+  board.markCellAtPosition(5, 'O');
+  board.markCellAtPosition(8, 'O');
+
+  expect(board.hasWin()).to.equal(false);
+});
+
+it('identifies a win', () => {
+  var board = new Board();
+
+  board.markCellAtPosition(1, 'X');
+  board.markCellAtPosition(2, 'X');
+  board.markCellAtPosition(3, 'X');
+
+  expect(board.hasWin()).to.equal(true);
+});
+
+it('identifies a tie', () => {
+  var board = new Board();
+
+  board.markCellAtPosition(1, 'X');
+  board.markCellAtPosition(2, 'X');
+  board.markCellAtPosition(6, 'X');
+  board.markCellAtPosition(7, 'X');
+  board.markCellAtPosition(9, 'X');
+
+  board.markCellAtPosition(3, 'O');
+  board.markCellAtPosition(4, 'O');
+  board.markCellAtPosition(5, 'O');
+  board.markCellAtPosition(8, 'O');
+
+  expect(board.hasTie()).to.equal(true);
 });
