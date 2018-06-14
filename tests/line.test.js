74557d5bc502f3755a5edd947ed13d935ae8d822
Laurent Curau <laurentcurau@gmail.com>
2018-06-14 10:26:00 +0100

    Formatting

--- tests/line.test.js
+++ tests/line.test.js
@@ -5,9 +5,7 @@ const Cell = require('../lib/cell.js');
 it('identifies a win', () => {
   let line = new Line(new Cell(1), new Cell(2), new Cell(3));
 
-  line.cells.forEach((cell) => {
-    cell.markWithSymbol('X');
-  });
+  line.cells.forEach((cell) => cell.markWithSymbol('X'));
 
   expect(line.hasWin()).to.equal(true);
 });
